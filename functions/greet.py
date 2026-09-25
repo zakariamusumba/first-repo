e6387213fe9b317c8b8228055b71f9a453a6ed94
zachattack99 <zakariamusumba98@gmail.com>
2026-09-25 09:05:49 +0300

    Add files via upload

---
+++ functions/greet.py
@@ -0,0 +1,13 @@
+def greet(name):
+    habari = "Good morning "
+    return habari + name
+
+def add(x,y):
+    result = x + y
+    print(result)
+    return add
+
+def sub(x,y):
+    result = x - y
+    print(result)
+    return sub
---
+++ functions/main.py
@@ -0,0 +1,11 @@
+from greet import greet
+
+name = input("Enter your name: ")
+welcome = greet(name)
+print(welcome)
+
+from greet import add
+add(6,5)
+
+from greet import sub
+sub(9,5)
